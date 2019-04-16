cc55ca53e7024234033a34eb9b99f564069fa8fe
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-16 09:06:39 +0200

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,17 +1,17 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unu=2080898295&redirect_uri=https%3A%2F%2Fdiscord.gg%2FcqMkUuc&scope=bot`;
-  message.channel.send(`${client.guilds}`);
+  message.channel.send(``);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: "User"
+  permLevel: "Bot Owner"
 };
 
 exports.help = {
-  name: "servers",
+  name: "test",
   category: "Miscelaneous",
-  description: "Shows the servers",
-  usage: "servers"
+  description: "a test command",
+  usage: "test"
 };
