581d650b756c31c2ea587e688d72434ad68cb51b
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-16 09:13:12 +0200

    Update and rename servers.js to test.js

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unu=2080898295&redirect_uri=https%3A%2F%2Fdiscord.gg%2FcqMkUuc&scope=bot`;
-  message.channel.send(``);
+  message.channel.send(`This Command is in testing`);
 };
 
 exports.conf = {
