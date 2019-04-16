0aefc07711b6854e63ec8c2884f1e76e24122178
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-16 07:45:19 +0200

    Create servers.js

---
+++ commands/servers.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unu=2080898295&redirect_uri=https%3A%2F%2Fdiscord.gg%2FcqMkUuc&scope=bot`;
+  message.channel.send(`${client.guilds});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "servers",
+  category: "Miscelaneous",
+  description: "Shows the servers",
+  usage: "servers"
+};
