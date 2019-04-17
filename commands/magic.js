ade516feda5ea21b33e9fd51fbc51bcc8c3b90fb
jay214421343 <48416316+jay214421343@users.noreply.github.com>
2019-04-17 16:32:24 +0200

    Update magic.js

--- commands/magic.js
+++ commands/magic.js
@@ -18,7 +18,7 @@ exports.conf = {
 
 exports.help = {
   name: "magic8",
-  category: "Miscelaneous",
+  category: "Fun",
   description: "Magic 8-Ball, does exactly what the toy does. (Results may vary)",
   usage: "<query:str>",
 };
